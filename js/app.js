801b074c6b8bfa6333136dd6a64c30fc050bd178
aninditaroy <anindita.roy.mu@gmail.com>
2022-02-16 23:26:29 +0600

    input-field and expense calc added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log('hello')
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    calculateExpenses();
+});
+
+function getInputValue(inputId) {
+    const input = document.getElementById(inputId);
+    const inputText = document.getElementById(inputId).value;
+    const inputField = parseInt(inputText);
+    // clear input field
+    input.value = '';
+    return inputField;
+}
+
+function calculateExpenses() {
+    const totalExpenses = document.getElementById('total-expenses');
+    const food = getInputValue('food');
+    const rent = getInputValue('rent');
+    const cloths = getInputValue('cloths');
+    const total = parseInt(food) + parseInt(rent) + parseInt(cloths);
+    totalExpenses.innerText = total;
+}
