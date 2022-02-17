47a7d534447e7bbdabdb04aef957ab170e1b9a3d
aninditaroy <anindita.roy.mu@gmail.com>
2022-02-17 09:33:06 +0600

    reset button added

--- js/app.js
+++ js/app.js
@@ -7,6 +7,10 @@ function getInputValue(inputId) {
     input.value = '';
     return inputField;
 }
+// get element by id
+function getElement(param){
+    return document.getElementById(param);
+}
 // calculate sum of expenses
 function calculateExpenses(newInputFood, newInputRent, newInputCloths) {
     const sumExpenses = newInputFood + newInputRent + newInputCloths;
@@ -15,13 +19,14 @@ function calculateExpenses(newInputFood, newInputRent, newInputCloths) {
 
 // calculate total expenses and balance
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    const totalExpenses = document.getElementById('total-expenses');
+    const totalExpenses = getElement('total-expenses');
     const income = getInputValue('income');
     const food = getInputValue('food');
     const rent = getInputValue('rent')
     const cloths = getInputValue('cloths')
-    const failMessage = document.getElementById('fail-text');
-    const failMessageTwo = document.getElementById('fail-text-2'); const balance = document.getElementById('balance');
+    const failMessage = getElement('fail-text');
+    const failMessageTwo = getElement('fail-text-2'); 
+    const balance = getElement('balance');
     if (isNaN(income) || isNaN(food) || isNaN(rent) || isNaN(cloths)) {
         failMessageTwo.style.display = 'none';
         failMessage.style.display = 'block';
@@ -35,6 +40,11 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
         totalExpenses.innerText = sumOfExpenses;
         balance.innerText = income - totalExpenses.innerText;
     }
-
 });
-
+// reset button
+document.getElementById('reset-btn').addEventListener('click',function(){
+    const totalExpenses = getElement('total-expenses');
+    const balance = getElement('balance');
+    totalExpenses.innerText = 0;
+    balance.innerText = 0;
+})
