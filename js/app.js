298dd8c1f67393aa1fdda38c185d948134ec7769
aninditaroy <anindita.roy.mu@gmail.com>
2022-02-17 09:09:14 +0600

    total expenses and balance calculation added

--- js/app.js
+++ js/app.js
@@ -1,31 +1,26 @@
-document.getElementById('calculate-btn').addEventListener('click', function () {
-    calculateExpenses();
-});
-
+// get input value
 function getInputValue(inputId) {
     const input = document.getElementById(inputId);
     const inputText = document.getElementById(inputId).value;
     const inputField = parseInt(inputText);
-    const failMessage = document.getElementById('fail-text');
-    const failMessageTwo = document.getElementById('fail-text-2');
-    if (inputField != 'number') {
-        failMessage.style.display = 'block';
-        failMessageTwo.style.display = 'none';
-    }
-    if (inputField < 0) {
-        failMessageTwo.style.display = 'block';
-        failMessage.style.display = 'none';
-    }
     // clear input field
     input.value = '';
     return inputField;
 }
-
-function calculateExpenses() {
-    const totalExpenses = document.getElementById('total-expenses');
-    const food = getInputValue('food');
-    const rent = getInputValue('rent');
-    const cloths = getInputValue('cloths');
-    const total = parseInt(food) + parseInt(rent) + parseInt(cloths);
-    totalExpenses.innerText = total;
+// calculate sum of expenses
+function calculateExpenses(newInputFood, newInputRent, newInputCloths) {
+    const sumExpenses = newInputFood + newInputRent + newInputCloths;
+    return sumExpenses;
 }
+// calculate total expenses and balance
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const totalExpenses = document.getElementById('total-expenses');
+    const income = getInputValue('income');
+    const food = getInputValue('food');
+    const rent = getInputValue('rent')
+    const cloths = getInputValue('cloths');
+    const sumOfExpenses = calculateExpenses(food, rent, cloths);
+    totalExpenses.innerText = sumOfExpenses;
+    balance.innerText = income - totalExpenses.innerText;
+});
+
